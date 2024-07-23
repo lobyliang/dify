42632ffeab3daef8b59d212ed0f9ae6c6dbf51c4
loby <lobyliang@qq.com>
2024-07-23 15:15:35 +0800

    修改多租户登录

--- web/app/signin/normalForm.tsx
+++ web/app/signin/normalForm.tsx
@@ -147,6 +147,10 @@ const NormalForm = () => {
     if (google) window.location.href = google.redirect_url;
   }, [google, google_error]);
 
+  useEffect(() => {
+    // console.log(IS_CE_EDITION, "IS_CE_EDITION");
+  });
+
   return (
     <>
       <div className="w-full mx-auto">
@@ -158,7 +162,7 @@ const NormalForm = () => {
 
       <div className="w-full mx-auto mt-8">
         <div className="bg-white ">
-          {!IS_CE_EDITION && (
+          {false && (
             <div className="flex flex-col gap-3 mt-6">
               <div className="w-full">
                 <a href={getPurifyHref(`${apiPrefix}/oauth/login/github`)}>
@@ -199,7 +203,7 @@ const NormalForm = () => {
             </div>
           )}
 
-          {IS_CE_EDITION && (
+          {false && (
             <>
               {/* <div className="relative mt-6">
                 <div className="absolute inset-0 flex items-center" aria-hidden="true">
@@ -241,7 +245,7 @@ const NormalForm = () => {
                     <span>{t("login.password")}</span>
                     {/* <Tooltip
                       selector='forget-password'
-                      htmlContent={
+                      htmlContent=
                         <div>
                           <div className='font-medium'>{t('login.forget')}</div>
                           <div className='font-medium text-gray-500'>
@@ -296,6 +300,101 @@ const NormalForm = () => {
               </form>
             </>
           )}
+          <>
+            {/* <div className="relative mt-6">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center text-sm">
+                  <span className="px-2 text-gray-300 bg-white">OR</span>
+                </div>
+              </div> */}
+
+            <form onSubmit={() => {}}>
+              <div className="mb-5">
+                <label
+                  htmlFor="email"
+                  className="my-2 block text-sm font-medium text-gray-900"
+                >
+                  {t("login.email")}
+                </label>
+                <div className="mt-1">
+                  <input
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    id="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder={t("login.emailPlaceholder") || ""}
+                    className={
+                      "appearance-none block w-full rounded-lg pl-[14px] px-3 py-2 border border-gray-200 hover:border-gray-300 hover:shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 placeholder-gray-400 caret-primary-600 sm:text-sm"
+                    }
+                  />
+                </div>
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="password"
+                  className="my-2 flex items-center justify-between text-sm font-medium text-gray-900"
+                >
+                  <span>{t("login.password")}</span>
+                  {/* <Tooltip
+                      selector='forget-password'
+                      htmlContent=
+                        <div>
+                          <div className='font-medium'>{t('login.forget')}</div>
+                          <div className='font-medium text-gray-500'>
+                            <code>
+                              sudo rm -rf /
+                            </code>
+                          </div>
+                        </div>
+                      }
+                    >
+                      <span className='cursor-pointer text-primary-600'>{t('login.forget')}</span>
+                    </Tooltip> */}
+                </label>
+                <div className="relative mt-1">
+                  <input
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") handleEmailPasswordLogin();
+                    }}
+                    type={showPassword ? "text" : "password"}
+                    autoComplete="current-password"
+                    placeholder={t("login.passwordPlaceholder") || ""}
+                    className={
+                      "appearance-none block w-full rounded-lg pl-[14px] px-3 py-2 border border-gray-200 hover:border-gray-300 hover:shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 placeholder-gray-400 caret-primary-600 sm:text-sm pr-10"
+                    }
+                  />
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+                    <button
+                      type="button"
+                      onClick={() => setShowPassword(!showPassword)}
+                      className="text-gray-400 hover:text-gray-500 focus:outline-none focus:text-gray-500"
+                    >
+                      {showPassword ? "👀" : "😝"}
+                    </button>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-2">
+                <Button
+                  tabIndex={0}
+                  type="primary"
+                  onClick={handleEmailPasswordLogin}
+                  disabled={isLoading}
+                  className="w-full !fone-medium !text-sm"
+                >
+                  {t("login.signBtn")}
+                </Button>
+              </div>
+            </form>
+          </>
           {/*  agree to our Terms and Privacy Policy. */}
           <div className="w-hull text-center block mt-2 text-xs text-gray-600">
             {t("login.tosDesc")}
